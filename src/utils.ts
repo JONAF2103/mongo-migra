15e7ef3c23cf8459adcbcf86f655725972a8ec61
Jonathan <jonathan.franco@halopowered.com>
2023-10-12 13:18:25 -0300

    Release 1.5.1

--- src/utils.ts
+++ src/utils.ts
@@ -42,9 +42,9 @@ async function executeCommand(command: string, environment: unknown = {}): Promi
 export async function transpileInMemory<T = any>(file: string, folder?: string): Promise<T> {
   try {
     if (folder) {
-      await executeCommand(`tsc ${folder}/**/**.ts`);
+      await executeCommand(`tsc ${resolve(folder)}/**/**.ts`);
     } else {
-      await executeCommand(`tsc ${file}`);
+      await executeCommand(`tsc ${resolve(file)}`);
     }
   } catch (ignored) {}
   let transpiledFile: string = file.replace('.ts', '.js');
